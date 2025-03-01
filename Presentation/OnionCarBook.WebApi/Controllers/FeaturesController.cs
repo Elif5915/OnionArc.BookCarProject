339b09c6dfc432757a5b137812cda328d3e323d0
Elif Yıldırım <elifyildirim3@outlook.com>
2025-03-02 00:21:36 +0300

    ..

---
+++ Presentation/OnionCarBook.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionCarBook.Application.Features.Mediator.Queries.FeatureQueries;
+
+namespace OnionCarBook.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class FeaturesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public FeaturesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> FeatureList()
+    {
+        var values = await _mediator.Send(new GetFeatureQuery());
+        return Ok(values);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdFeature(int id)
+    {
+       var value = await _mediator.Send(new GetFeatureByIdQuery(id));
+        return Ok(value);
+    }
+}
