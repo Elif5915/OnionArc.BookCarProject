95d180d610a5653cbfca1aad5df1f7d606b2a6cc
Elif Yıldırım <elifyildirim3@outlook.com>
2025-05-19 22:59:53 +0300

    Frontend devam

---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.ViewComponents.UILayoutViewComponents;
+
+public class _HeadUILayoutComponentPartial : ViewComponent
+{
+    //Invoke : çağırmak,yakalamak
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
